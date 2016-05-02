079aed73c77f120649639f41e07a069b8d9eb39c
dstineback <dstineback@gmail.com>
2016-05-02 13:27:16 -0700

    added map, centered locatoin, and zoom

--- scripts/map.js
+++ scripts/map.js
@@ -2,7 +2,7 @@
 function initMap(){
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
-    center: {lat: 47.61, lng: 122.33},
-    zoom: 18
+    center: {lat: 47.61, lng: -122.33},
+    zoom: 14
   });
 }
