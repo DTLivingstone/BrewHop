a1a01e9ce5d5ab6366c2b4d8b5afa49b495f2973
David Livingstone <david@david.tl>
2016-05-02 20:35:45 -0700

    added route and getTweets function

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-///////////////
-app.get('/*', function(req, res){
-  res.send('Hello World');
-});
-///////////////
+/// ROUTES ///
+app.use(express.static('./'));
 
+app.get('/twit', function(request, response) {
+  getTweets();
+});
+
+app.get('*', function(request, response) {
+  response.sendFile('index.html', { root: '.' });
+});
+
+/// TWIT ///
 var Twit = require('twit');
 
 var T = new Twit({
@@ -19,10 +25,13 @@ var T = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-T.post('statuses/update', { status: 'Hello from twit, a Twitter client for node.js' }, function(err, data, response) {
-  console.log(data);
-});
+var getTweets = function() {
+  T.get('statuses/user_timeline', { screen_name: 'hilliardsbeer', count: 3 }, function(err, data, response) {
+    console.log(data);
+  });
+};
 
+/// START SERVER ///
 app.listen(port, function() {
-  console.log('Server started on port ' + port + '!');
+  console.log('Express server started on port ' + port);
 });
