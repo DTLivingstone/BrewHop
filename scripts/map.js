1ac234a72ad18f558b7447e334ca5f6fd1d91a4d
dstineback <dstineback@gmail.com>
2016-05-03 14:37:17 -0700

    added a new icon but needs to be resized. Also added some breweries to the list

--- scripts/map.js
+++ scripts/map.js
@@ -1,22 +1,28 @@
-var marker;
-var mapIconInfo = "brewery info goes here";
+var BrewMap = {};
+var marker =[];
+var map;
+var mapIconInfo = "brewery infor goes here";
+var breweries = [
+//list brewery long & lat in the brackets
+  ['Bad Jimmy'/'s Brewing', 47.660403, -122.365406, 4],
+  ['Big Al Brewing',47.514322, -122.352378, 5],
+  ['Big Time Brewing Company', 47.657854, -122.313546, 6],
+  ['Burdick Brewery', 47.530388, -122.323241, 7],
+  ['Cloudburst Brewing', 47.611686, -122.345118, 8],
+  ['503B S Michigan St, Seattle, WA 98108', 47.545501, -122.328096, 9],
+  ['two-beers', 47.61, -122.33, 1],
+  ['test-beers', 47.69, -122.36,2],
+  ['hillards', 47.64, -122.34, 3],
 
-function initMap(){
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
+];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.61, lng: -122.33},
     zoom: 15
   });
   setMarkers(map);
-}
-
-var breweries = [
-//list brewery long & lat in the brackets
-  ['two-beers', 47.61, -122.33, 1],
-  ['test-beers', 47.69, -122.36,2],
-
-];
-
+};
 
 function setMarkers(map) {
   var infowindow = new google.maps.InfoWindow({
@@ -24,14 +30,13 @@ function setMarkers(map) {
     maxWidth: 200
   });
   var image = {
-    url: '/images/icons/icon-hopfen.svg',
-    //the marker size is 20 px wide by 30px tall
-    size: new google.maps.Size(20,30),
+    url: 'images/icons/hop-map-icon.png',
+    size: new google.maps.Size(80,80),
     origin: new google.maps.Point(0,0),
-    anchor: new google.maps.Point(0,30)
+    anchor: new google.maps.Point(0,20)
   };
   var shape = {
-    coords: [1,1,1,20,18,20,18,1],
+    coords: [1,40,60,80,78,60,40,1],
     type: 'poly'
   };
   for (var i = 0; i < breweries.length; i++){
@@ -48,4 +53,4 @@ function setMarkers(map) {
       infowindow.open(map, marker);
     });
   }
-}
+};
