dc5316d8947f2c355e85315baa71ec7286d49fd8
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-05-03 13:40:36 -0700

    add webDB and sql to construct brewery table using unique brewery ids array for location endpoint

--- scripts/brewery.js
+++ scripts/brewery.js
@@ -1,45 +1,88 @@
-function Brewery (opts) {
-  this.name = opts.name;
-  this.id = opts.id;
-}
+// (function(module) {
+  function Brewery (opts) {
+    Object.keys(opts).forEach(function(b, index, keys) {
+      this[b] = opts[b];
+    }, this);
+  }
 
-Brewery.all = [];
+  Brewery.all = [];
+  Brewery.ids = [];
 
-//TODO: Perhaps move this function to the server side (server reads our json file?).
-Brewery.getAll = function() {
-  $.getJSON('/data/breweries.json', function(data) {
-    Brewery.loadById(data);
-    localStorage.breweries = JSON.stringify(data);
-  });
-};
-
-//TODO: Perhaps move this function to the server side (server reads our json file?).
-Brewery.loadById = function(data) {
-  Brewery.all = data.map(function(ele) {
-    return new Brewery(ele);
-  });
-};
-
-Brewery.requestLocationById = function(id) {
-  //TODO: Refactor by passing in an id rather than iterating over Brewery.all
-  Brewery.all.forEach(function(b) {
-    var breweryId = b.id;
-    $.get('/locations/' + breweryId, function(data) {
-      console.log(data);
+  var breweryIds = function() {
+    $.getJSON('/data/breweries.json', function(data) {
+      data.forEach(function(b){
+        if (Brewery.ids.indexOf(b.id) === -1) {
+          Brewery.ids.push(b.id);
+        }
+      });
     });
-  });
-};
+  };
 
-Brewery.requestSocialById = function(id) {
-  //TODO: Refactor by passing in an id rather than iterating over Brewery.all
-  Brewery.all.forEach(function(b) {
-    var breweryId = b.id;
-    $.get('/socialaccounts/' + breweryId, function(data) {
-      console.log(data);
+  Brewery.prototype.handleLocationEndpoint = function() {
+    breweryIds();
+    Brewery.ids.forEach(function(id) {
+      $.get('/locations/' + this, function(data) {
+        Brewery.insertLocationRecord();
+      });
     });
-  });
-};
+  };
 
-Brewery.getAll();
-Brewery.requestLocationById();
-Brewery.requestSocialById();
+  Brewery.createTable = function(callback) {
+    webDB.execute(
+      'CREATE TABLE IF NOT EXISTS breweries (' +
+        'id PRIMARY KEY, ' +
+        'name VARCHAR(255), ' +
+        'description TEXT, ' +
+        'website VARCHAR(255), ' +
+        'established DATE, ' +
+        'isOrganic BOOLEAN, ' +
+        'streetAddress VARCHAR(255), ' +
+        'phone VARCHAR(255), ' +
+        'latitude FLOAT, ' +
+        'longitude FLOAT, ' +
+        'openToPublic BOOLEAN, ' +
+        'hoursOfOperation VARCHAR(255));'
+    );
+  };
+
+  Brewery.loadAll = function(rows) {
+    Brewery.all = rows.map(function(ele) {
+      return new Brewery(ele);
+    });
+  };
+
+  Brewery.fetchAll = function(callback) {
+    webDB.execute('SELECT * FROM breweries', function(rows) {
+      if (rows.length) {
+        Brewery.loadAll(rows);
+        callback();
+      } else {
+        $getJSON('data/breweries.json', function(rawData) {
+          rawData.forEach(function(breweryEle) {
+            Brewery.insertRecord();
+          });
+          webDB.execute('SELECT * FROM breweries', function(rows) {
+            Brewery.loadAll(rows);
+            callback();
+          });
+        });
+      }
+    });
+  };
+
+  Brewery.prototype.insertLocationRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO breweries (id, streetAddress, postalCode, phone, latitude, longitude, openToPublic, hoursOfOperation) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+          'data': [this.id, this.streetAddress, this.postalCode, this.phone, this.latitude, this.longitude, this.openToPublic, this.hoursOfOperation],
+        }
+      ],
+      callback
+    );
+  };
+
+  Brewery.createTable();
+
+  // module.Brewery = Brewery;
+// })(window);
--- server.js
+++ server.js
@@ -3,25 +3,25 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-/// TWIT ///
-var Twit = require('twit');
-
-var T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-
-var getTweets = function() {
-  T.get('statuses/user_timeline', { screen_name: 'hilliardsbeer', count: 3 }, function(err, data, response) {
-    console.log(data);
-  });
-};
-
-/// START SERVER ///
-app.listen(port, function() {
-  console.log('Express server started on port ' + port);
+// /// TWIT ///
+// var Twit = require('twit');
+//
+// var T = new Twit({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// });
+//
+// var getTweets = function() {
+//   T.get('statuses/user_timeline', { screen_name: 'hilliardsbeer', count: 3 }, function(err, data, response) {
+//     console.log(data);
+//   });
+// };
+//
+// /// START SERVER ///
+// app.listen(port, function() {
+//   console.log('Express server started on port ' + port);
 
 /// BreweryDB ///
 var proxyBreweryLocation = function(req, res) {
@@ -38,9 +38,9 @@ var proxyBrewerySocial = function(req, res) {
 /// ROUTES ///
 app.use(express.static('./'));
 
-app.get('/twit', function(request, response) {
-  getTweets();
-});
+// app.get('/twit', function(request, response) {
+//   getTweets();
+// });
 
 app.get('/locations/*', proxyBreweryLocation);
 app.get('/socialaccounts/*', proxyBrewerySocial);
