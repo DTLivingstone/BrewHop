51afa17177500149b2f5bd1e70b50e886bbe25fb
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-05-03 15:31:49 -0700

    breweries table with unique breweryid filled

--- scripts/brewery.js
+++ scripts/brewery.js
@@ -21,21 +21,59 @@
   Brewery.handleLocationEndpoint = function() {
     Brewery.ids.forEach(function(id) {
       $.get('/locations/' + id, function(data) {
-        Brewery.all.push(new Brewery(data));
+        var breweryInstance = new Brewery(data);
+        breweryInstance.insertLocationRecord();
       });
     });
+  };
+
+  Brewery.loadAll = function(rows) {
+    Brewery.all = rows.map(function(b) {
+      return new Brewery(b);
+    });
+  };
+
+  Brewery.idRecord = function() {
+    Brewery.ids.forEach(function(id) {
+      Brewery.insertBreweryId(id);
+    });
+  };
+
+  Brewery.insertBreweryId = function(id) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO breweries (breweryId) VALUES (?)',
+          'data': [id],
+        }
+      ]
+    );
+  };
+
+  Brewery.prototype.insertLocationRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO breweries (streetAddress, postalCode, phone, latitude, longitude, openToPublic, hoursOfOperation) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+          'data': [this.streetAddress, this.postalCode, this.phone, this.latitude, this.longitude, this.openToPublic, this.hoursOfOperation],
+        }
+      ],
+      callback
+    );
   };
 
   Brewery.createTable = function(callback) {
     webDB.execute(
       'CREATE TABLE IF NOT EXISTS breweries (' +
-        'id PRIMARY KEY, ' +
+        'id INTEGER PRIMARY KEY, ' +
         'name VARCHAR(255), ' +
+        'breweryId, ' +
         'description TEXT, ' +
         'website VARCHAR(255), ' +
         'established DATE, ' +
         'isOrganic BOOLEAN, ' +
         'streetAddress VARCHAR(255), ' +
+        'postalCode, ' +
         'phone VARCHAR(255), ' +
         'latitude FLOAT, ' +
         'longitude FLOAT, ' +
@@ -64,12 +102,12 @@
     });
   };
 
-  Brewery.insertLocationRecord = function(callback) {
+  Brewery.findWhere = function(field, value, callback) {
     webDB.execute(
       [
         {
-          'sql': 'INSERT INTO breweries (id, streetAddress, postalCode, phone, latitude, longitude, openToPublic, hoursOfOperation) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-          'data': [this.id, this.streetAddress, this.postalCode, this.phone, this.latitude, this.longitude, this.openToPublic, this.hoursOfOperation],
+          sql: 'SELECT * FROM breweries WHERE ' + field + ' = ?;',
+          data: [value]
         }
       ],
       callback
