654186833e6ce8e699d3f4d272a8f4a3ab230db8
dstineback <dstineback@gmail.com>
2016-05-03 10:23:52 -0700

    changed the size of the map and added key to map api script

--- scripts/map.js
+++ scripts/map.js
@@ -3,7 +3,7 @@ function initMap(){
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
     center: {lat: 47.61, lng: -122.33},
-    zoom: 14
+    zoom: 15
   });
   // setMarkers(map);
 }
