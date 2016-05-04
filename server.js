1a5d4ecdfa683aea6134d038599616ee9b6335c4
David Livingstone <david@david.tl>
2016-05-04 13:03:45 -0700

    route set up to pass phone number to server

--- server.js
+++ server.js
@@ -3,21 +3,39 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// /// Twit ///
-// var Twit = require('twit');
-//
-// var T = new Twit({
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   app_only_auth: true
-// });
-//
-// var getTweets = function(req, res) {
-//   T.get('statuses/user_timeline', {screen_name: req.params[0], count: 3})
-//   .done(function(result) {
-//     res.send(result.data);
-//   });
-// };
+/// Twit ///
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  app_only_auth: true
+});
+
+var getTweets = function(req, res) {
+  T.get('statuses/user_timeline', {screen_name: req.params[0], count: 3})
+  .done(function(result) {
+    res.send(result.data);
+  });
+};
+
+/// Yelp ///
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
+});
+
+var searchYelp = function(req, res) {
+  console.log('yelp search for', req.params[0]);
+  yelp.phoneSearch({ phone: req.params[0] })
+  .then(function (data) {
+    console.log(data);
+  });
+};
 
 /// BreweryDB ///
 var proxyBreweryLocation = function(req, res) {
@@ -37,8 +55,8 @@ var proxyBreweryName = function(req, res) {
 /// Routes ///
 app.use(express.static('./'));
 
-// app.get('/twit/*', getTweets);
-
+app.get('/twit/*', getTweets);
+app.get('/yelp/*', searchYelp);
 app.get('/locations/*', proxyBreweryLocation);
 app.get('/name/*', proxyBreweryName);
 
